563fcf74b27eb72da873cab93ce762b411d4646b
CleversonAbreu <crehbatera@gmail.com>
2021-12-08 05:21:20 -0300

    :tada: initial commit

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{useState,useRef} from 'react';
+import { StyleSheet, Image, Text, View,TextInput,TouchableOpacity,SafeAreaView, Keyboard } from 'react-native';
+import api from './src/services/api';
+import { TextInputMask } from 'react-native-masked-text';
 
 export default function App() {
+  const [cep,setCep] = useState('');
+  const inputRef = useRef(null);
+  const [cepUser,setCepUser] = useState(null);  
+
+  async function getZipcode(){
+    if(cep==''){
+      alert('Digite um cep válido')
+      return;
+    }
+
+    try {
+      const response = await api.get('/'+cep+'/json');
+      setCepUser(response.data);
+      Keyboard.dismiss();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function clear(){
+    setCep('')
+    inputRef.current.focus();
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{alignItems:'center'}}>
+      <Image source={require('./assets/correios_logo.jpeg')} style={styles.img}/>
+        <TextInputMask
+        placeholder='83601-970'
+            style={styles.input}
+            type={'zip-code'}
+            value={cep}
+            onChangeText={(text)=>setCep(text)}          
+        />
+      </View>
+      <View style={styles.areaBtn}>
+        <TouchableOpacity style={[styles.btn,{backgroundColor: '#FF9900'}]}onPress={getZipcode}>
+          <Text style={styles.btnText}>Consultar</Text>
+        </TouchableOpacity>
+      </View>
+      
+      { cepUser &&
+            <View style={styles.result}>
+            <Text style={styles.itemText}>ENDEREÇO:</Text>
+            <Text style={styles.itemText}>{cepUser.logradouro}</Text>
+            <Text style={styles.itemText}>{cepUser.bairro}</Text>
+            <Text style={styles.itemText}>{cepUser.localidade}/{cepUser.uf}</Text>
+          </View>
+      }     
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +62,51 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:20
   },
+  input:{
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    width: '90%',
+    padding : 10,
+    fontSize: 18
+  },
+  areaBtn:{
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 15,
+    justifyContent: 'space-around'
+  },
+  btn:{
+    height: 55,
+    width: 330,
+    marginTop: 15,
+    justifyContent:'center',
+    alignItems: 'center',
+    padding: 15,
+    elevation: 2,
+    shadowColor: '#52006A',
+  },
+  btnText:{
+    fontSize:18,
+    color: '#fff'
+  },
+  result:{
+    flex: 1,
+    marginTop: 10,
+    padding: 15,
+    alignItems:'flex-start'
+  },
+  itemText:{
+    fontSize:20
+  },
+  img:{
+    marginTop: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 200,
+    width: 300
+  }
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+# Zipcode R
+A simple react native example for API consumption using axios.
+
+<p float="left">
+  <img src="http://kaktuscoder.com.br/img/zipcode_r1.jpeg" height="450" />
+  <img src="http://kaktuscoder.com.br/img/zipcode_r2.jpeg" height="450" /> 
+</p>
